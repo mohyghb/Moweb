6ba5a0a371898eb398cdfe3ee065f4e3edd127a4
moh <moh10moh78@gmail.com>
2021-02-15 12:01:56 -0800

    fixed the problem with search suggestions having html contents in them.

--- app/src/main/java/com/moofficial/moweb/Moweb/MoSearchEngines/GoogleSearchEngine.java
+++ app/src/main/java/com/moofficial/moweb/Moweb/MoSearchEngines/GoogleSearchEngine.java
@@ -1,5 +1,7 @@
 package com.moofficial.moweb.Moweb.MoSearchEngines;
 
+import android.text.Html;
+
 import com.moofficial.moessentials.MoEssentials.MoString.MoString;
 import com.moofficial.moweb.Moweb.MoSearchEngines.MoSearchAutoComplete.MoSuggestion;
 import com.moofficial.moweb.Moweb.MoSearchEngines.MoSearchAutoComplete.MoSuggestions;
@@ -14,20 +16,20 @@ public class GoogleSearchEngine extends MoSearchEngine {
     private static final String SPLITTER = "suggestion data=";
 
     public GoogleSearchEngine() {
-        super(GOOGLE_SEARCH,GOOGLE_ADDRESS,SEARCH_SUGGESTIONS);
+        super(GOOGLE_SEARCH, GOOGLE_ADDRESS, SEARCH_SUGGESTIONS);
     }
 
 
     @Override
-    protected MoSuggestions getSuggestions(MoSuggestions suggestions,String html) {
+    protected MoSuggestions getSuggestions(MoSuggestions suggestions, String html) {
         String[] ls = html.split(SPLITTER);
-        for(int i = 1; i < ls.length; i++){
+        for (int i = 1; i < ls.length; i++) {
             // second index of \"
             int index = ls[i].indexOf("\"", 1);
-            if(index>1) {
-                String suggestion = ls[i].substring(1,index);
-                float similarity = MoString.getSimilarity(suggestion,suggestions.getSearch(),true);
-                suggestions.add(new MoSuggestion(suggestion,similarity));
+            if (index > 1) {
+                String suggestion = Html.fromHtml(ls[i].substring(1, index)).toString();
+                float similarity = MoString.getSimilarity(suggestion, suggestions.getSearch(), true);
+                suggestions.add(new MoSuggestion(suggestion, similarity));
             }
         }
         return suggestions;
